d74f8cb6058529e00b53e65acacd4ccc96ab8d3a
Lascoski <nataly_lascoski@hotmail.com>
2025-11-05 20:24:18 -0300

    atualizações

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import Component from './Component.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App title="Os perigos de roubo de dados na internet" content="conteúdo recebido com  "/>
+    <App title="Os perigos de roubo de dados na internet ." content="conteúdo recebido com  "/>
     <App title="" content="mesmo conteúdo recebido com  props.title"/>
     <Component />
   </StrictMode>,
 )
+
